cdbe9b578c4c5a13aa52ed2a5a60565205a99565
dakamar084 <dacamar084@gmail.com>
2026-02-12 17:53:19 +0100

    aaaaa

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ function resto() {
         <iframe 
             width="560" 
             height="315" 
-            src="https://www.youtube.com/embed/${videoSeleccionado}?autoplay=1" 
+            src="https://www.youtube.com/embed/${videoSeleccionado}?autoplay=1&mute=1&loop=1" 
             title="YouTube video player" 
             frameborder="0" 
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
